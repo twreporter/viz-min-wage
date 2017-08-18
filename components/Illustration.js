13060662cf262030b7f0183d70b87fa9db0bc98e
garfieldduck <garfieldduck@gmail.com>
2017-08-18 20:35:22 +0800

    [Update] Add animations on illustrations changed

--- components/Illustration.js
+++ components/Illustration.js
@@ -5,7 +5,7 @@ import React from 'react'
 import anime from 'animejs'
 import { connect } from 'react-redux'
 import { get, has, map } from 'lodash'
-import { screen } from '../styles/utils'
+import { scaleInAnimation } from '../styles/common-variables'
 import illustrationData from '../constants/illustrationData'
 import styled from 'styled-components'
 
@@ -15,9 +15,9 @@ const SvgWrapper = styled.div`
   height: 100%;
   top: 0;
   left: 0;
-  transition: all 0.5s ease-in-out;
-  transform: scale(0.8);
-  opacity: 0;
+  transition: ${scaleInAnimation.transition};
+  transform: ${scaleInAnimation.hide.transform};
+  opacity: ${scaleInAnimation.hide.opacity};
 
   svg {
     max-width: 100%;
@@ -31,24 +31,13 @@ const _ = {
   map,
 }
 
-const cssSettings = {
-  show: {
-    opacity: 1,
-    transform: 'scale(1)',
-  },
-  hide: {
-    opacity: 0,
-    transform: 'scale(0.8)',
-  },
-}
-
 class Illustration extends React.Component {
   render() {
     const { sectionKey } = this.props
     const SvgIllustrations = _.map(illustrationData, (svg, key) => {
       const isShown = sectionKey === key
       return (
-        <SvgWrapper key={`i-${key}`} style={isShown ? cssSettings.show : cssSettings.hide}>
+        <SvgWrapper key={`i-${key}`} style={isShown ? scaleInAnimation.show : scaleInAnimation.hide}>
           {svg.component}
         </SvgWrapper>
       )
